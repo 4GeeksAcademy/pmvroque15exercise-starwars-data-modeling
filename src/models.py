aeb1cee956bdac2138677003c51e50aa8ccb92a4
Pat Roque <169104185+pmvroque15@users.noreply.github.com>
2024-11-12 02:07:23 +0000

    Finished

--- src/models.py
+++ src/models.py
@@ -6,24 +6,49 @@ from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
+#Sarwars
+    #User
+    #Characters
+    #Planets
+    #Starships
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(550), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+    characters =  relationship('Characters', back_populates='user')
+    planets = relationship('Planets', back_populates='user')
+    starships = relationship('Starships', back_populates='user')
+
+class Characters(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50), nullable=False)
+    birth_year = Column(String(50))
+    gender = Column(String(50))
+
+    userid = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    terrain = Column(String(250))
+    population  = Column(Integer)
+
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Starships(Base):
+    __tablename__ = 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    
+    user_id = Column(Integer, ForeignKey('user.id'))
 
     def to_dict(self):
         return {}
